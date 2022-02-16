6783a383a62940aa1bbed71a78c0cfce3de6b99f
navneeeth <navneeth.padaki15@gmail.com>
2022-02-15 18:58:19 -0800

    few additions

--- main.py
+++ main.py
@@ -21,8 +21,29 @@ time_data = []
 desc = []
 entries = []
 exits = []
-
+stations_list = []
+lines_list = []
+lines_cache = ''
+stations_cache = ''
 time1 = time.time()
+
+def checkStationsList(val):
+    if(val not in stations_list):
+        stations_list.append(val)
+
+def checkLines(val):
+    values = [char for char in val]
+    for i in values:
+        if i not in lines_list:
+            lines_list.append(i)
+            lines_cache = val
+def readTxtFile(filepath):
+    ccfile = open(filepath, "r")
+    flag = 1
+    flag2 = 1
+    for aline in ccfile:
+        values = aline.split(',')
+    ccfile.close()
 for filename in os.listdir(dirname):
     f = os.path.join(dirname, filename)
     # checking if it is a file
@@ -41,24 +62,27 @@ for filename in os.listdir(dirname):
                     #print(values[-2])
                     #print(values[-1])
                 flag2 = 0
+                checkStationsList(values[3])
+                
+                if(not lines_cache == values[4]):
+                    checkLines(values[4])
+                
                 entries.append(int(values[-2]))
                 exits.append(int(values[-1]))
+                
             #print(values)
         ccfile.close()
         #loaded_text = np.loadtxt(f, skiprows=1)
         #print(loaded_text)
 
-print(len(entries))
-count = 1
-print(type(entries[0]))
-for j in exits:
-    if(type(j) == str):
-        print(j)
-        print(count)
-    count = count + 1
-print(sum(entries))
-print(sum(exits))
-print(sum(entries)-sum(exits))
+#print(len(entries))
+#count = 1
+#print(type(entries[0]))
 
+#print(sum(entries))
+#print(sum(exits))
+print(sum(entries)-sum(exits))
+print(len(stations_list))
+print(len(lines_list))
 time2 = time.time()
 print(time2-time1)
